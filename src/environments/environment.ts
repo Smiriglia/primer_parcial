7324b570d7f37c7ab92ac98d89d245c5ab4c371e
smiriglia <faustosmiriglia@gmail.com>
2024-05-30 18:10:34 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    firebaseConfig: { "projectId": "primer-parcial-ec919",
+     "appId": "1:1025388251761:web:6bdd8f4510c4e0d7ed29d0",
+     "storageBucket": "primer-parcial-ec919.appspot.com",
+     "apiKey": "AIzaSyCUW8Wal4TY4e6lFVXfvdlrayRmrJ7Mkns",
+     "authDomain": "primer-parcial-ec919.firebaseapp.com",
+     "messagingSenderId": "1025388251761" },
+};
